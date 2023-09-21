64e02928ee0ff8b25c7be7ff0faf099658191166
caahcavalcantte <cacavalcantte@gmail.com>
2023-09-21 02:29:03 -0300

    finalizando projeto

--- src/app/components/card/card-pricing/card-pricing.component.ts
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -1,19 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-pricing',
   templateUrl: './card-pricing.component.html',
   styleUrls: ['./card-pricing.component.css']
 })
-export class CardPricingComponent implements OnInit{
+export class CardPricingComponent {
 
-@Input()
-gameType:string="Digital PS4"
-gamePrice:string="R$ 399,90"
-constructor(){}
+  @Input()
+  gameType: string = ""
+  @Input()
+  gamePrice: string = ""
+  constructor() { }
 
-ngOnInit(): void {
+  ngOnInit(): void {
 
-}
+  }
 
 }
